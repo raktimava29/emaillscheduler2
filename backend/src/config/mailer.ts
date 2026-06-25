72f1e354ba7aa3afaee9bfdbfe12becdde896b00
Raktimava Bhattacharyya <raktim.bhatta29@gmail.com>
2026-06-26 03:09:17 +0530

    change

--- backend/src/config/mailer.ts
+++ backend/src/config/mailer.ts
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.ethereal.email",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.ETHEREAL_USER!,
     pass: process.env.ETHEREAL_PASS!,
